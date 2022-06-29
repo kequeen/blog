234a816111c66214d47fcaafe44040cf4b52cee9
liangnian <liangnian@baidu.com>
2022-06-29 09:48:09 +0800

    网站大纲

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
 const { defaultTheme } = require('@vuepress/theme-default')
 module.exports = {
+  port : 80,
   theme: defaultTheme({
+    // 你也可以直接将它设置为一个 URL
+    repo: 'https://github.com/kequeen/blog',
+    docsBranch: 'master',
+    docsDir: 'docs',
     // 侧边栏数组
     // 所有页面会使用相同的侧边栏
     sidebar: [
@@ -17,11 +22,44 @@ module.exports = {
           },
         ],
       },
-      // 字符串 - 页面文件路径
-        {
+      {
         text: '软技能',
         link: '/soft/',
-      }
+      },
+      {
+        text: '算法',
+        link: '/algorithm/',
+        children: [
+          // SidebarItem
+          {
+            text: '关于排序',
+            link: '/algorithm/sort.md',
+            children: [],
+          },
+          {
+            text: 'leetcode',
+            link: '/algorithm/leetcode/',
+            children: [],
+          },
+        ],
+      },
+      {
+        text: '编程语言',
+        link: '/language/',
+        children: [
+          // SidebarItem
+          {
+            text: '关于go',
+            link: '/language/go/',
+            children: [
+                {
+                    text : '与其它语言的对比',
+                    link : '/language/go/compare.md',
+                }
+            ],
+          },
+        ],
+      },
     ],
   }),
 }
